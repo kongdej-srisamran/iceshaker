bfceecdbe5762e1abe6950118c63cbc17730ab5a
kongdej <kongdejs@gmail.com>
2019-08-20 21:03:25 +0700

    updated

--- liff.js
+++ liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = '5fe83910-3522-44e3-85c9-13f9b017b72f'; // LED, Button
+const USER_SERVICE_UUID         = '5f961675-3050-4cef-8a98-2e7f58fb4e43'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
